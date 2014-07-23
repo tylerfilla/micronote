39ab1fe21b4d8c3c51e9ddc75555f9433c3ee72f
tylerfilla <tylerfilla@gmail.com>
2014-07-23 14:37:28 -0500

    onCreate(Bundle) must call super

--- src/com/gmail/tylerfilla/android/notes/NotesActivity.java
+++ src/com/gmail/tylerfilla/android/notes/NotesActivity.java
@@ -7,6 +7,7 @@ public class NotesActivity extends Activity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_notes);
     }
     
