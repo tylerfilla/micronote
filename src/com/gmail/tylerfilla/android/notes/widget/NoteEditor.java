fc2a3850f0358142cf7d6562d21b69fec590dce3
tylerfilla <tylerfilla@gmail.com>
2014-07-31 14:55:07 -0500

    Must re-implement note editor format, but this will be immensely beneficial

--- src/com/gmail/tylerfilla/android/notes/widget/NoteEditor.java
+++ src/com/gmail/tylerfilla/android/notes/widget/NoteEditor.java
@@ -8,10 +8,17 @@ import com.gmail.tylerfilla.android.notes.core.Note;
 
 public class NoteEditor extends WebView {
     
+    private static final String internalCode = "<html><body onLoad=\"editarea.document.designMode = 'on';\" style=\"width: 100%; height: 100%;\">"
+            + "<iframe name=\"editarea\" id=\"editarea\" style=\"border: none; position: absolute; top: 0; left: 0; width: 100%; height: 100%;\"></iframe>"
+            + "</body></html>";
+    
     private Note note;
     
     public NoteEditor(Context context, AttributeSet attrs) {
         super(context, attrs);
+        
+        this.getSettings().setJavaScriptEnabled(true);
+        this.loadData(internalCode, "text/html", "UTF-8");
     }
     
     public Note getNote() {
