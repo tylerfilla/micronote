549d88b24dc20a8641773923cc3cc6f511011462
tylerfilla <tylerfilla@gmail.com>
2014-08-10 22:58:42 -0500

    Re-implemented metrics reporting, reactivating line drawing

--- assets/editor.js
+++ assets/editor.js
@@ -1,14 +1,20 @@
 
 // Written by Tyler Filla
 
-var headerBar = document.getElementById("headerBar");
-var contentArea = document.getElementById("contentArea");
-var metricsElement = document.getElementById("metricsElement");
+var headerBar;
+var contentArea;
+var metricsElement;
 
 // Events
 
 window.onload = function() {
+	headerBar = document.getElementById("headerBar");
+	contentArea = document.getElementById("contentArea");
+	metricsElement = document.getElementById("metricsElement");
+	
 	reportMetrics();
+	
+	metricsElement.parentNode.removeChild(metricsElement);
 }
 
 // Reporting
@@ -18,5 +24,31 @@ function report(message) {
 }
 
 function reportMetrics() {
-	report("lineWidth=4.0");
+	var lineWidth = 0;
+	var lineHeight = 0;
+	var lineOffsetX = 0;
+	var lineOffsetY = 0;
+	var contentHeight = 0;
+	
+	while (lineWidth <= 0) {
+		lineWidth = contentArea.clientWidth*window.devicePixelRatio;
+	}
+	while (lineHeight <= 0) {
+		lineHeight = metricsElement.clientHeight*window.devicePixelRatio;
+	}
+	while (lineOffsetX <= 0) {
+		lineOffsetX = metricsElement.getBoundingClientRect().left*window.devicePixelRatio;
+	}
+	while (lineOffsetY <= 0) {
+		lineOffsetY = (metricsElement.getBoundingClientRect().top + window.pageYOffset - metricsElement.ownerDocument.documentElement.clientTop)*window.devicePixelRatio;
+	}
+	while (contentHeight <= 0) {
+		contentHeight = contentArea.offsetHeight*window.devicePixelRatio;
+	}
+	
+	report("lineWidth=" + lineWidth);
+	report("lineHeight=" + lineHeight);
+	report("lineOffsetX=" + lineOffsetX);
+	report("lineOffsetY=" + lineOffsetY);
+	report("contentHeight=" + contentHeight);
 }
