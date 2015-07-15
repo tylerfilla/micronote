e58a1b82d40cd26ac0b016a70abdc7e2227dfd8f
tylerfilla <tylerfilla@gmail.com>
2015-07-15 17:32:19 -0500

    Added official IabHelper

--- app/src/main/java/com/gmail/tylerfilla/android/notes/activity/ActivityPurchase.java
+++ app/src/main/java/com/gmail/tylerfilla/android/notes/activity/ActivityPurchase.java
@@ -16,10 +16,6 @@ public class ActivityPurchase extends Activity {
     
     private static final int PURCHASE_REQUEST_CODE = -5897;
     
-    private static final int DEVELOPER_PAYLOAD_GENERATOR_MAX_ITERATIONS = 20;
-    private static final int DEVELOPER_PAYLOAD_GENERATOR_MIN_ITERATIONS = 5;
-    private static final String DEVELOPER_PAYLOAD_GENERATOR_SEED = "++I am a seed! YAY!++3.141592653589793238462643383";
-    
     private String productId;
     
     private BillingHelper billingHelper;
@@ -83,12 +79,9 @@ public class ActivityPurchase extends Activity {
         // Set up billing helper
         this.billingHelper.setup();
         
-        // Generate a random developer payload
-        final String developerPayload = this.generateDeveloperPayload();
-        
         // Attempt to make purchase
         try {
-            this.billingHelper.purchase(this, PURCHASE_REQUEST_CODE, this.productId, BillingHelper.ProductType.IN_APP, developerPayload, new BillingHelper.PurchaseCallback() {
+            this.billingHelper.purchase(this, PURCHASE_REQUEST_CODE, this.productId, BillingHelper.ProductType.IN_APP, null, new BillingHelper.PurchaseCallback() {
                 
                 @Override
                 public void onPurchaseCompleted(BillingHelper.Response response, BillingHelper.PurchaseInfo purchaseInfo) {
@@ -108,36 +101,6 @@ public class ActivityPurchase extends Activity {
         } catch (BillingHelper.BillingHelperException e) {
             e.printStackTrace();
         }
-    }
-    
-    private String generateDeveloperPayload() {
-        // Create pRNG
-        Random random = new Random(SystemClock.uptimeMillis());
-        
-        // Calculate number of iterations
-        int numIterations = DEVELOPER_PAYLOAD_GENERATOR_MIN_ITERATIONS + random.nextInt(DEVELOPER_PAYLOAD_GENERATOR_MAX_ITERATIONS - DEVELOPER_PAYLOAD_GENERATOR_MIN_ITERATIONS);
-        
-        // Start with seed
-        String payloadString = DEVELOPER_PAYLOAD_GENERATOR_SEED;
-        
-        // Generate payload
-        for (int i = 0; i < numIterations; i++) {
-            // Generate a mutation
-            char mutation = (char) (random.nextInt() & 0xFFFF);
-            
-            // Mutate all characters
-            for (int ci = 0; ci < payloadString.length(); ci++) {
-                if (ci == 0) {
-                    payloadString = String.valueOf((char) (payloadString.charAt(0) ^ mutation)) + payloadString.substring(1, payloadString.length());
-                } else if (ci == payloadString.length() - 1) {
-                    payloadString = payloadString.substring(0, payloadString.length() - 1) + String.valueOf((char) (payloadString.charAt(payloadString.length() - 1) ^ mutation));
-                } else {
-                    payloadString = payloadString.substring(0, ci) + String.valueOf((char) (payloadString.charAt(ci) ^ mutation)) + payloadString.substring(ci + 1, payloadString.length());
-                }
-            }
-        }
-        
-        return payloadString;
     }
     
 }
